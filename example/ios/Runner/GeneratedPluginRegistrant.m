6471882f4f9a62f5b5e652dc492d6ac4fa9f80a4
c0deslinger <yusuf.fachroni@gmail.com>
2025-10-01 12:47:58 +0700

    update version

--- example/ios/Runner/GeneratedPluginRegistrant.m
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -6,23 +6,9 @@
 
 #import "GeneratedPluginRegistrant.h"
 
-#if __has_include(<particle_base/ParticleBasePlugin.h>)
-#import <particle_base/ParticleBasePlugin.h>
-#else
-@import particle_base;
-#endif
-
-#if __has_include(<particle_connect/ParticleConnectPlugin.h>)
-#import <particle_connect/ParticleConnectPlugin.h>
-#else
-@import particle_connect;
-#endif
-
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
-  [ParticleBasePlugin registerWithRegistrar:[registry registrarForPlugin:@"ParticleBasePlugin"]];
-  [ParticleConnectPlugin registerWithRegistrar:[registry registrarForPlugin:@"ParticleConnectPlugin"]];
 }
 
 @end
